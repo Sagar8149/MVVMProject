2fb190e523f50cb25423d3fd37f11d0deeeef1d6
sagar <https://sagar_deshmukh_emerway@bitbucket.org/sagar_deshmukh_emerway/androidprojectbitbucket.git>
2019-09-04 16:37:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mvvmproject/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.mvvmproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.mvvmproject.databinding.ActivityMainBinding;
+import com.example.mvvmproject.model.User;
+
+public class MainActivity extends AppCompatActivity {
+
+    private User user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+//        setContentView(R.layout.activity_main);
+
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+
+        user = new User();
+        user.setName("Ravi Tamada");
+        user.setEmail("ravi@androidhive.info");
+
+        binding.setUser(user);
+
+    }
+}
